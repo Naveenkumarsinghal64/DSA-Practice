16c1be04f3871322ece252661fc261fac3a7a182
Naveen Kumar Singhal <singhalnaveen64@gmail.com>
2026-06-19 17:43:25 +0530

    Time: 7 ms (19.15%), Space: 9.6 MB (92.36%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int n=s.size(),m=t.size();
-        if(n!=m){
-            return false;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        if(s==t){
+            return true;
         }
-        vector<int> v1(26),v2(26);
-        for(int i=0;i<n;i++){
-            v1[s[i]-'a']++;
-            v2[t[i]-'a']++;
-        }
-        return v1==v2;
+        return false;
     }
 };
