4502de88da0fc192eab89388e625f1589ea7ef5f
Naveen Kumar Singhal <singhalnaveen64@gmail.com>
2026-06-19 18:23:40 +0530

    Time: 31 ms (5.07%), Space: 41.7 MB (14.98%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int q=-1;
+    bool found=false;
+    void findMax(vector<int> nums, int a,int b){
+        found=false;
+        q=-1;
+        for(int i=b+1;i<nums.size();i++){
+            if(nums[i]>a){
+                q=nums[i];
+                found=true;
+                break;
+            }
+        }
+    }
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        int a=0,b=0;
+        for(int i=0;i<nums1.size();i++){
+            for(int j=0;j<nums2.size();j++){
+                if(nums1[i]==nums2[j]){
+                    a=nums2[j];
+                    b=j;
+                    findMax(nums2,a,b);
+                }
+            }
+            if(found==true){
+                nums1[i]=q;
+            }
+            else{
+                nums1[i]=q;
+            }
+        }
+        return nums1;
+    }
+};
