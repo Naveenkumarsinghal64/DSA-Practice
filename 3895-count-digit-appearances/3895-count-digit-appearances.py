62e0b8190557a4c860cf79e297dd914b783d2e17
Naveen Kumar Singhal <singhalnaveen64@gmail.com>
2026-07-25 00:00:13 +0530

    Time: 10 ms (94.85%), Space: 12.6 MB (58.82%) - LeetHub

---
+++ 3895-count-digit-appearances/3895-count-digit-appearances.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def countDigitOccurrences(self, nums, digit):
+        """
+        :type nums: List[int]
+        :type digit: int
+        :rtype: int
+        """
+        ans="".join(map(str,nums))
+        return ans.count(str(digit))
